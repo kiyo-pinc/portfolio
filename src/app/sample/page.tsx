b15cd87e9c97eac686b4453649e11a3eeaa13e23
Kiyota Kyosuke <kiyota.hanabiya@gmail.com>
2024-02-26 17:26:11 +0900

    使用していないimportの削除

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Sample() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Sample</h2>
     </main>
-  );
+  )
 }
