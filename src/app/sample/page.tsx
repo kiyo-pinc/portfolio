75d679dd3fc795dc25cff43f75ff48edf660ed44
Kiyota Kyosuke <kiyota.hanabiya@gmail.com>
2024-02-26 17:04:05 +0900

    add config

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Sample() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Portfolio</h1>
+      <h2>Sample</h2>
     </main>
   );
 }
